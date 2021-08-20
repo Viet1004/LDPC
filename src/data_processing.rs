4858dbd33a7252a927d680fe9576975b856d0460
Anne Marin <marin@veriqloud.fr>
2021-08-20 19:08:11 +0200

    test ldpc decoding

--- src/data_processing.rs
+++ src/data_processing.rs
@@ -1,8 +1,8 @@
 extern crate rand;
 extern crate sprs;
-use rand::{seq::SliceRandom, SeedableRng};
-use rand::thread_rng;
+//use rand::thread_rng;
 use rand::Rng;
+use rand::{seq::SliceRandom, SeedableRng};
 use rand_chacha::ChaChaRng;
 //use std::borrow::Borrow;
 //use std::cell::RefCell;
@@ -11,28 +11,70 @@ use rand_chacha::ChaChaRng;
 //use ndarray::Array1;
 //use ndarray::Array2;
 
-use sprs::{CsMat,CsVec};
+//use sprs::{CsMat, CsVec};
+use sprs::CsMat;
 
 // TODO: seed is unused
 
-pub fn bsc_channel(n:usize ,code: &Vec<usize>, proba: f64) ->(CsVec<usize>, Vec<f64>){
-    let mut rng = rand::thread_rng();
-    let mut received: Vec<usize> = Vec::new();
-    let mut post_proba : Vec<f64> = Vec::new();
-    let mut code_clone = code.clone();
-    for _i in 0..code.len(){
-        if rng.gen::<f64>() < proba{
-            code_clone[_i] ^= 1;
+// added functions
+pub fn csmat_dot(mat: &CsMat<usize>, vec: &Vec<usize>) -> Vec<usize> {
+    let indptr = mat.indptr();
+    let indices = mat.indices();
+    let (m, n) = mat.shape();
+    let mut mult = Vec::with_capacity(m);
+
+    for i in 0..m {
+        //let r = indices[indptr[i]..indptr[i + 1]];
+        //println!("r: {:?}", indptr.outer_inds_sz(i));
+        let range = indptr.outer_inds_sz(i);
+        let mut res = 0;
+        for j in range {
+            res += vec[indices[j]];
         }
-        if code_clone[_i] == 1{
-            received.push(_i);
-            post_proba.push(proba);
-        }else{post_proba.push(1.0-proba);}
-    } 
-    let data = vec![1;received.len()];
-    (CsVec::new(n,received,data),post_proba)
+        mult.push(res % 2);
+    }
+
+    //println!("mult is {:?}", mult);
+    mult
 }
 
+//
+
+// pub fn bsc_channel(n: usize, code: &Vec<usize>, proba: f64) -> (CsVec<usize>, Vec<f64>) {
+//     let mut rng = rand::thread_rng();
+//     let mut received: Vec<usize> = Vec::new();
+//     let mut post_proba: Vec<f64> = Vec::new();
+//     let mut code_clone = code.clone();
+//     for _i in 0..code.len() {
+//         if rng.gen::<f64>() < proba {
+//             code_clone[_i] ^= 1;
+//         }
+//         if code_clone[_i] == 1 {
+//             received.push(_i);
+//             post_proba.push(proba);
+//         } else {
+//             post_proba.push(1.0 - proba);
+//         }
+//     }
+//     let data = vec![1; received.len()];
+//     (CsVec::new(n, received, data), post_proba)
+// }
+
+pub fn bsc_channel(n: usize, codeword: &Vec<usize>, proba: f64) -> (Vec<usize>, Vec<f64>) {
+    let mut rng = rand::thread_rng();
+    let n = codeword.len();
+    let mut received: Vec<usize> = codeword.clone();
+    for i in 0..n {
+        if rng.gen::<f64>() < proba {
+            received[i] ^= 1;
+        }
+    }
+    let post_proba = received.iter().map(|&x| if x == 1 { proba } else { 1.0 - proba }).collect();
+
+    //let data = vec![1; received.len()];
+    //(CsVec::new(n, received, data), post_proba)
+    (received, post_proba)
+}
 
 pub fn make_matrix_regular_ldpc(w_c: usize, w_r: usize, n: usize, seed: u8) -> CsMat<usize> {
     if (n * w_c) % w_r != 0 {
@@ -42,145 +84,153 @@ pub fn make_matrix_regular_ldpc(w_c: usize, w_r: usize, n: usize, seed: u8) -> C
     // TODO: what is this used for ?
     let num_row: usize = n * w_c / w_r as usize;
     let mut indices: Vec<usize> = (0..n).collect();
-    let mut indptr : Vec<usize> = vec![0;num_row+1];
-    let data: Vec<usize> = vec![1;n*w_c];
+    let mut indptr: Vec<usize> = vec![0; num_row + 1];
+    let data: Vec<usize> = vec![1; n * w_c];
     let indices_copy: Vec<usize> = indices.clone();
-    for i in 0..(num_row+1){
-        indptr[i as usize] = i*w_r;
+    for i in 0..(num_row + 1) {
+        indptr[i as usize] = i * w_r;
     }
-    for i in 0..(w_c-1){
+    for i in 0..(w_c - 1) {
         let mut temp = indices_copy.clone();
-//        temp.shuffle(&mut thread_rng());
+        //        temp.shuffle(&mut thread_rng());
         let seed_list = [seed * i as u8; 32];
         let mut rng = ChaChaRng::from_seed(seed_list);
         temp.shuffle(&mut rng);
-        for j in 0..(n/w_r){
-            &temp[j*w_r..(j+1)*w_r].sort_unstable();
-            
+        for j in 0..(n / w_r) {
+            &temp[j * w_r..(j + 1) * w_r].sort_unstable();
         }
         indices.append(&mut temp);
-    }    
-    CsMat::new((num_row,n),
-                    indptr,
-                    indices,
-                    data)
+    }
+    CsMat::new((num_row, n), indptr, indices, data)
 }
 
-fn input_regular_ldpc(m:usize, n:usize,matrix: &CsMat<usize>, post_proba: &Vec<f64>) -> CsMat<f64>{
-//    let (indptr, indices, _) = matrix.into_raw_storage();
+fn input_regular_ldpc(m: usize, n: usize, matrix: &CsMat<usize>, post_proba: &Vec<f64>) -> CsMat<f64> {
+    //    let (indptr, indices, _) = matrix.into_raw_storage();
     let nnz = matrix.nnz();
-    let mut indptr_clone: Vec<usize> = vec![0;m+1];    // Need to check it if there is a logic error
-    for index in 0..m{
+    let mut indptr_clone: Vec<usize> = vec![0; m + 1]; // Need to check it if there is a logic error
+    for index in 0..m {
         indptr_clone[index] = matrix.indptr().outer_inds_sz(index).start;
     }
-//    println!("{:?}",indptr_clone);    
+    //    println!("{:?}",indptr_clone);
     indptr_clone[m] = nnz;
     let mut indices: Vec<usize> = Vec::new();
-    for index in matrix.indices(){
+    for index in matrix.indices() {
         indices.push(*index);
     }
     let mut data = Vec::new();
-    for _i in matrix.indices().iter(){
-        let temp = post_proba[*_i]/(1.0-post_proba[*_i]);
+    for _i in matrix.indices().iter() {
+        let temp = post_proba[*_i] / (1.0 - post_proba[*_i]);
         data.push(temp.ln());
     }
-//    println!("indptr_clone{:?}", indptr_clone);
-    CsMat::new((m,n),
-                indptr_clone,
-                indices,
-                data)
-} 
-
-pub fn message_passing(matrix: &mut CsMat<usize>,syndrome: CsVec<usize>,post_proba:Vec<f64>,number_of_iter: usize ) 
-                -> Option<CsVec<usize>> {
-    let (m,n) = matrix.shape();
-    let mut success = false;
-    let mut matrix_input = input_regular_ldpc(m, n, matrix, &post_proba);
-    let mut syndrome_vec : Vec<usize> = vec![0;m];
-    for i in syndrome.indices(){
-        syndrome_vec[*i] = 1;
-        println!("i is {}", *i);
-    }
-    for _i in 0..number_of_iter{
-        horizontal_run(&mut matrix_input, &syndrome_vec);
-        let code_word = vertical_run(&mut matrix_input, &post_proba, n, m);
-        success = verification(matrix, &code_word, &syndrome);
-        match success{
-            true => return Some(code_word),
-            false => continue
-        }
-    }
-    return None;
+    //    println!("indptr_clone{:?}", indptr_clone);
+    CsMat::new((m, n), indptr_clone, indices, data)
 }
 
-fn horizontal_run(matrix: &mut CsMat<f64>, syndrome: &Vec<usize>) {
-    let (m,_) = matrix.shape();  
+fn horizontal_run(matrix: &mut CsMat<f64>, syndrome: &[usize]) {
+    let (m, _) = matrix.shape();
     let nnz = matrix.nnz();
-    let mut indptr_clone: Vec<usize> = vec![0;m+1];
-    for index in 0..m{
+    let mut indptr_clone: Vec<usize> = vec![0; m + 1];
+    for index in 0..m {
         indptr_clone[index] = matrix.indptr().outer_inds_sz(index).start;
-    }    
+    }
     indptr_clone[m] = nnz;
     let data = matrix.data_mut();
-//    println!("Before horizontal run: {:?}", data);
-    for index in 0..nnz{
-        data[index] = (data[index]/2.0).tanh();
+    //    println!("Before horizontal run: {:?}", data);
+    for index in 0..nnz {
+        data[index] = (data[index] / 2.0).tanh();
     }
-    for index in 0..m{
-        let temp: f64 = data[indptr_clone[index]..indptr_clone[index+1]].iter().product();
-        for _i in indptr_clone[index]..indptr_clone[index+1]{
-            let mut temp1 : f64 = 0.0;        
-            if syndrome[index] == 1{
-                temp1 = -temp/data[_i];
-            }else{
-                temp1 = temp/data[_i];
+    for index in 0..m {
+        let temp: f64 = data[indptr_clone[index]..indptr_clone[index + 1]].iter().product();
+        for _i in indptr_clone[index]..indptr_clone[index + 1] {
+            let mut temp1: f64 = 0.0;
+            if syndrome[index] == 1 {
+                temp1 = -temp / data[_i];
+            } else {
+                temp1 = temp / data[_i];
             }
-            let temp2 = (1.0+temp1)/(1.0-temp1);
+            let temp2 = (1.0 + temp1) / (1.0 - temp1);
             data[_i] = temp2.ln();
         }
     }
-//    println!("After Horizontal run: {:?}", data);
+    //    println!("After Horizontal run: {:?}", data);
 }
 
-fn vertical_run(matrix: &mut CsMat<f64>, post_proba: &Vec<f64>, n: usize,m:usize) -> CsVec<usize>{
+fn vertical_run(matrix: &mut CsMat<f64>, post_proba: &Vec<f64>, n: usize, m: usize) -> Vec<usize> {
     let mut indices = Vec::new();
 
     let mut matrix_temp = matrix.to_csc();
     let nnz = matrix.nnz();
-    let mut indptr_clone: Vec<usize> = vec![0;n+1];
-    for index in 0..n{
+    let mut indptr_clone: Vec<usize> = vec![0; n + 1];
+    for index in 0..n {
         indptr_clone[index] = matrix_temp.indptr().outer_inds_sz(index).start;
-    }    
+    }
     indptr_clone[n] = nnz;
     let data = matrix_temp.data_mut();
-//    println!("Data before vertical run: {:?}", data);
-    for index in 0..n{
-        let mut temp: f64 = data[indptr_clone[index]..indptr_clone[index+1]].iter().sum();
+    //    println!("Data before vertical run: {:?}", data);
+    for index in 0..n {
+        let mut temp: f64 = data[indptr_clone[index]..indptr_clone[index + 1]].iter().sum();
         temp += post_proba[index];
-        for _i in indptr_clone[index]..indptr_clone[index+1]{        
+        for _i in indptr_clone[index]..indptr_clone[index + 1] {
             data[_i] = temp - data[_i]
         }
-        if temp <= 0.0{
+        if temp <= 0.0 {
             indices.push(index);
         }
     }
-    let data_vec = vec![1;indices.len()];
+    //let data_vec = vec![1; indices.len()];
     let matrix_temp = matrix_temp.to_csr();
     let data = matrix.data_mut();
-    for datum_ind in 0..nnz{
+    for datum_ind in 0..nnz {
         data[datum_ind] = matrix_temp.data()[datum_ind];
     }
-//    println!("data after vertical run:{:?}", data);
-    CsVec::new(n, indices,data_vec)
+    //    println!("data after vertical run:{:?}", data);
+    //CsVec::new(n, indices, data_vec)
+    let mut recv = vec![0; n];
+    for i in 0..indices.len() {
+        recv[indices[i]] = 1;
+    }
+    recv
 }
 
-fn verification(matrix0: &CsMat<usize>, code_word: &CsVec<usize>, syndrome: &CsVec<usize>) -> bool {
+fn verification(matrix0: &CsMat<usize>, received_vec: &Vec<usize>, syndrome: &[usize]) -> bool {
     // Maybe there's a better way to do this, but here's what I used:
     // https://stackoverflow.com/questions/66925648/how-do-i-create-a-two-dimensional-array-from-a-vector-of-vectors-with-ndarray
-    if matrix0 * code_word == *syndrome{
-        true
-    }else{false}
+    // if matrix0 * received_vec == *syndrome {
+    //     true
+    // } else {
+    //     false
+    // }
+    &csmat_dot(matrix0, received_vec) == syndrome
 }
+
+pub fn message_passing(matrix: &mut CsMat<usize>, syndrome: &[usize], post_proba: Vec<f64>, number_of_iter: usize) -> (bool, Vec<usize>) {
+    let (m, n) = matrix.shape();
+    let mut success = false;
+    let mut matrix_input = input_regular_ldpc(m, n, matrix, &post_proba);
+    // let mut syndrome_vec: Vec<usize> = vec![0; m];
+    // for i in syndrome.indices() {
+    //     syndrome_vec[*i] = 1;
+    //     println!("i is {}", *i);
+    // }
+    let mut received_vec = vec![0; n];
+    for i in 0..number_of_iter {
+        horizontal_run(&mut matrix_input, &syndrome);
+        received_vec = vertical_run(&mut matrix_input, &post_proba, n, m);
+        success = verification(matrix, &received_vec, &syndrome);
+        match success {
+            true => {
+                println!("nb of iteration: {:?}", i + 1);
+                //return Some(received_vec);
+                return (true, received_vec);
+            }
+            false => continue,
+        }
+    }
+
+    //return None;
+    (false, received_vec)
+}
+
 //#[cfg(test)]
 //mod tests{
 //    use super::*;
@@ -190,26 +240,25 @@ fn verification(matrix0: &CsMat<usize>, code_word: &CsVec<usize>, syndrome: &CsV
 //    }
 //}
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
-//    #[test]
-//    fn demo() {
-//        let n = 1000000;
-//        let w_c = 4;
-//        let w_r = 8;
-//        let crossover_proba = 0.02;
-//        let seed =10;
-//        let mut original_code_word : Vec<usize> = vec![0;n];
-//        let mut matrix = make_matrix_regular_ldpc(w_c, w_r, n, seed);
-//        let (received, post_proba) = bsc_channel(n, &mut original_code_word, crossover_proba);
-//        let syndrome = &matrix * &received;
-//        match message_passing(&mut matrix, syndrome, post_proba, 60){
-//            Some(_value) => {
-//                assert_eq!(2,2);
-//            }
-//            None => assert_eq!(2,3)
-//        }
-//    }
+    //    #[test]
+    //    fn demo() {
+    //        let n = 1000000;
+    //        let w_c = 4;
+    //        let w_r = 8;
+    //        let crossover_proba = 0.02;
+    //        let seed =10;
+    //        let mut original_code_word : Vec<usize> = vec![0;n];
+    //        let mut matrix = make_matrix_regular_ldpc(w_c, w_r, n, seed);
+    //        let (received, post_proba) = bsc_channel(n, &mut original_code_word, crossover_proba);
+    //        let syndrome = &matrix * &received;
+    //        match message_passing(&mut matrix, syndrome, post_proba, 60){
+    //            Some(_value) => {
+    //                assert_eq!(2,2);
+    //            }
+    //            None => assert_eq!(2,3)
+    //        }
+    //    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,275 @@
 mod data_processing;
-use sprs::CsVec;
+//use sprs::{CsMat, CsVec};
 
-use std::time::{Duration, Instant};
+use rand::Rng;
+use std::time::Instant;
+
+use std::env;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    use sprs::{CsMat, CsMatBase, CsMatView};
+    use sprs::{CsVecBase, CsVecViewI};
+    #[test]
+    fn trysparse() {
+        // Creating a sparse owned vector
+        let owned = CsVec::new(10, vec![0, 4], vec![-4, 2]);
+        // Creating a sparse borrowing vector with `I = u16`
+        let borrow = CsVecViewI::new(10, &[0_u16, 4], &[-4, 2]);
+        // Creating a general sparse vector with different storage types
+        let mixed = CsVecBase::new(10, &[0_u64, 4] as &[_], vec![-4, 2]);
+        println!("{:?} {:?} {:?}", owned, borrow, mixed);
+    }
+
+    #[test]
+    fn trysmat() {
+        // This creates an owned matrix
+        let owned_matrix = CsMat::new((2, 2), vec![0, 1, 1], vec![1], vec![4_u8]);
+        // This creates a matrix which only borrows the elements
+        let borrow_matrix = CsMatView::new((2, 2), &[0, 1, 1], &[1], &[4_u8]);
+        // A combination of storage types may also be used for a
+        // general sparse matrix
+        let mixed_matrix = CsMatBase::new((2, 2), &[0, 1, 1] as &[_], vec![1_i64].into_boxed_slice(), vec![4_u8]);
+    }
+
+    #[test]
+    fn trymul() {
+        let matrix = CsMat::new((2, 2), vec![0, 2, 4], vec![0, 1, 0, 1], vec![4, 5, 1, 3]);
+        //[[4,5],[1,3]
+        let vec = CsVec::new(2, vec![0, 1], vec![-4, 2]);
+        //[-4.2]
+
+        assert_eq!(&matrix * &vec, CsVec::new(2, vec![0, 1], vec![-6, 2]));
+        //rec: [-6,2]
+        println!("res: {:?}", &matrix * &vec);
+    }
+
+    #[test]
+    fn tryeq() {
+        let v1 = vec![1; 10];
+        let v2 = vec![1; 10];
+        if &v1 == &v2 {
+            println!("true");
+        } else {
+            println!("false");
+        }
+
+        let v1 = vec![1; 10];
+        let v2 = vec![0; 10];
+        if &v1 == &v2 {
+            println!("true");
+        } else {
+            println!("false");
+        }
+    }
+
+    use rand::seq::SliceRandom;
+    use rand::Rng;
+    #[test]
+    fn tryrand() {
+        let mut rng = rand::thread_rng();
+        let n = 10;
+        let time0 = Instant::now();
+        let mut y = vec![0; n];
+        y.append(&mut vec![1; n]);
+        println!("y: {:?}", y);
+        y.shuffle(&mut rng);
+        println!("Shuffled:   {:?}", y);
+        let time1 = Instant::now();
+        println!("time shuffle: {:?}", time1.saturating_duration_since(time0));
+
+        let time0 = Instant::now();
+        let mut x = Vec::with_capacity(2 * n);
+        for _ in 0..2 * n {
+            x.push(rng.gen_range(0..2));
+        }
+        println!("x: {:?}", x);
+        let time1 = Instant::now();
+        println!("time for gen_range: {:?}", time1.saturating_duration_since(time0));
+
+        let time0 = Instant::now();
+        let mut z = vec![0; 2 * n];
+        z = z.iter().map(|&x| rng.gen_range(0..2)).collect();
+        println!("z: {:?}", z);
+
+        let time1 = Instant::now();
+        println!("time for gen_range 2: {:?}", time1.saturating_duration_since(time0));
+    }
+
+    #[test]
+    fn trycodeword() {
+        let n = 1000000;
+        let mut rng = rand::thread_rng();
+        //let original_code_word: Vec<usize> = vec![0; n];
+
+        let time0 = Instant::now();
+        let mut indices = Vec::new();
+        for i in 0..n {
+            if rng.gen::<f64>() < 0.5 {
+                indices.push(i);
+            }
+        }
+        let nnz = indices.len();
+        //println!("codeword indices: {:?} nnz: {}", indices, nnz);
+        let sparse_vec = CsVec::new(n, indices, vec![1; nnz]);
+        //println!("sparse vec : {:?}", sparse_vec);
+
+        let time1 = Instant::now();
+        println!("time for gen_range 2: {:?}", time1.saturating_duration_since(time0));
+
+        let time0 = Instant::now();
+        let mut indices: Vec<usize> = (0..n).collect();
+        indices.shuffle(&mut rng);
+        let nnz = n / 2;
+        indices.truncate(nnz);
+        indices.sort();
+        //println!("indices:   {:?}", indices);
+        let sparse_vec = CsVec::new(n, indices, vec![1; nnz]);
+        //println!("sparse vec : {:?}", sparse_vec);
+
+        let time1 = Instant::now();
+        println!("time shuffle: {:?}", time1.saturating_duration_since(time0));
+
+        // keep codeword as a vec + indices
+
+        let time0 = Instant::now();
+        let mut codeword = vec![0; n];
+        let mut indices = vec![0; n];
+        let mut indices = Vec::new();
+        for i in 0..n {
+            if rng.gen::<f64>() < 0.5 {
+                codeword[i] = 1;
+                indices.push(i);
+            }
+        }
+        let nnz = indices.len();
+        //println!("codeword indices: {:?} nnz: {}", indices, nnz);
+        let sparse_vec = CsVec::new(n, indices, vec![1; nnz]);
+        //println!("sparse vec : {:?}", sparse_vec);
+
+        let time1 = Instant::now();
+        println!("time 3: {:?}", time1.saturating_duration_since(time0));
+    }
+}
 
 fn main() {
-    let n = 100000;
+    let mut time0;
+    let mut time1;
+
+    // let n = 12;
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("usage: cargo run <n>");
+        return;
+    }
+    let n: usize = args[1].parse().expect("arg n must be an integer");
     let w_c = 3;
     let w_r = 6;
     let n = w_r * (n / w_r);
-    println!("n={}", n);
+    let m = w_c * (n / w_r);
     let crossover_proba = 0.05;
     let seed = 10;
-    let mut original_code_word: Vec<usize> = vec![0; n];
-    let sparse_vec = CsVec::new(n, vec![0; 0], vec![1; 0]);
-    let mut time0 = Instant::now();
-    let (received, post_proba) = data_processing::bsc_channel(n, &mut original_code_word, crossover_proba);
-    let mut time1 = Instant::now();
-    println!("time to generate receive vector: {:?}", time1.saturating_duration_since(time0));
+
+    println!("n={} m={} w_c={} w_r={} p={}", n, m, w_c, w_r, crossover_proba);
+
+    let mut rng = rand::thread_rng();
+    time0 = Instant::now();
+    let mut codeword: Vec<usize> = vec![0; n];
+    let mut indices = Vec::new();
+    for i in 0..n {
+        if rng.gen::<f64>() < 0.5 {
+            codeword[i] = 1;
+            indices.push(i);
+        }
+    }
+    //let nnz = indices.len();
+    //println!("codeword: {:?}", codeword);
+    //println!("codeword indices: {:?} nnz: {}", indices, nnz);
+    //let sparse_vec = CsVec::new(n, indices, vec![1; nnz]);
+    //println!("sparse vec : {:?}", sparse_vec);
+    time1 = Instant::now();
+    println!("time to generate codeword: {:?}", time1.saturating_duration_since(time0));
+
+    // let sparse_vec = CsVec::new(n, vec![0; 0], vec![1; 0]);
 
     //   let a = CsMat::new_csc((9,12),
     //                     vec![0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
     //                     vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 2, 4, 7, 3, 5, 9, 11, 1, 6, 8, 10, 2, 4, 6, 9, 5, 7, 10, 11, 0, 1, 3, 8],
     //                     vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
     //   println!("Hello!");
+
     time0 = Instant::now();
     let mut matrix = data_processing::make_matrix_regular_ldpc(w_c, w_r, n, seed);
     time1 = Instant::now();
+    //println!("matrix: {:?}", matrix);
+    //println!("matrix {:?}", matrix.to_dense());
     println!("time to generate matrix: {:?}", time1.saturating_duration_since(time0));
+    //println!("matrix indice: {:?}", matrix);
 
-    //   println!("matrix indice: {:?}", matrix);
-    let syndrome = &matrix * &sparse_vec;
-    //   println!("syndrome: {:?}",syndrome.indices());
-    //   println!("received: {:?}", received);
+    // time0 = Instant::now();
+    // let mut syndrome = &matrix * &sparse_vec;
+    // time1 = Instant::now();
+    // println!("time to compute syndrome 1: {:?}", time1.saturating_duration_since(time0));
+    // time0 = Instant::now();
+    // //println!("syndrome: {:?}", syndrome);
+    // syndrome = syndrome.map(|&x| x % 2); // should then extrac only the non zero value..
+
+    // //println!("syndrome: {:?}", syndrome);
+    // //println!("syndrome: {:?}", syndrome.indices());
+    // //println!("received: {:?}", received);
+    // time1 = Instant::now();
+    // println!("time to compute syndrome +: {:?}", time1.saturating_duration_since(time0));
+
     time0 = Instant::now();
-    match data_processing::message_passing(&mut matrix, syndrome, post_proba, 60) {
-        Some(value) => {
-            println!("That is great but not quite!");
-            //         assert_eq!(value.indices(), original_code_word)
-        }
-        None => {
-            println!("Sorry mate!")
-        }
-    }
+    let mut syndrome2 = data_processing::csmat_dot(&matrix, &codeword);
     time1 = Instant::now();
-    println!("time to (not) decode: {:?}", time1.saturating_duration_since(time0));
+    println!("time to compute syndrome 2: {:?}", time1.saturating_duration_since(time0));
 
-    //   println!("Hello");
+    //assert_eq!(syndrome2, syndrome.data());
+
+    let nbTest = 10;
+    let mut cnt_success = 0;
+    for _ in 0..nbTest {
+        time0 = Instant::now();
+        let (mut received, post_proba) = data_processing::bsc_channel(n, &codeword, crossover_proba);
+        //println!("recv: {:?}", received);
+        let initial_errors: usize = received.iter().zip(codeword.iter()).map(|(&x, &y)| x ^ y).sum();
+        println!("initial errors {}", initial_errors);
+        time1 = Instant::now();
+        println!("time to generate receive vector: {:?}", time1.saturating_duration_since(time0));
+
+        time0 = Instant::now();
+        let mut success: bool;
+        let res = data_processing::message_passing(&mut matrix, &syndrome2, post_proba, 60);
+        success = res.0;
+        received = res.1;
+        //println!("decode: {:?}", received);
+        //{
+        // Some(value) => {
+        //     println!("That is great but not quite!");
+        //     //         assert_eq!(value.indices(), original_code_word)
+
+        //
+        // }
+        // None => {
+        //     println!("Sorry mate!")
+        // }
+        //}
+        if success {
+            cnt_success += 1;
+            println!("success");
+        } else {
+            println!("failure");
+        }
+
+        let final_errors: usize = received.iter().zip(codeword.iter()).map(|(&x, &y)| x ^ y).sum();
+        println!("final erros: {}", final_errors);
+
+        time1 = Instant::now();
+        println!("time to (not) decode: {:?}", time1.saturating_duration_since(time0));
+    }
+    println!("success: {}/{}", cnt_success, nbTest);
 }
