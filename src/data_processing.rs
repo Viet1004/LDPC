77fca6621a92f728ef8606c5722d32aae2c64bd8
Viet1004 <nguyenquocviet1004@gmail.com>
2021-08-20 15:40:11 +0200

    Working with all vector. Not optimizedgit add .!

--- src/data_processing.rs
+++ src/data_processing.rs
@@ -34,10 +34,6 @@ pub fn bsc_channel(n:usize ,code: &Vec<usize>, proba: f64) ->(CsVec<usize>, Vec<
 }
 
 pub fn make_matrix_regular_ldpc(w_c: usize, w_r: usize, n: usize, seed: u8) -> CsMat<usize> {
-    if (n * w_c) % w_r != 0 {
-        panic!("number of col * weight of col must be divisible by weight of row");
-    }
-
     // TODO: what is this used for ?
     let num_row: usize = n * w_c / w_r as usize;
     let mut indices: Vec<usize> = (0..n).collect();
@@ -96,18 +92,29 @@ pub fn message_passing(matrix: &mut CsMat<usize>,syndrome: CsVec<usize>,post_pro
     let mut success = false;
     let mut matrix_input = input_regular_ldpc(m, n, matrix, &post_proba);
     let mut syndrome_vec : Vec<usize> = vec![0;m];
+    let mut post_proba_log:Vec<f64> = vec![0.0;n];
+    for i in 0..n{
+        post_proba_log[i] = (post_proba[i]/(1.0-post_proba[i])).ln();
+    }
+//    println!("Post proba is {:?}", post_proba_log);
+//    println!("Matrix input is {:?}", matrix_input);
+    
     for i in syndrome.indices(){
         syndrome_vec[*i] = 1;
-        println!("i is {}", *i);
+//        println!("i is {}", *i);
     }
     for _i in 0..number_of_iter{
         horizontal_run(&mut matrix_input, &syndrome_vec);
-        let code_word = vertical_run(&mut matrix_input, &post_proba, n, m);
-        success = verification(matrix, &code_word, &syndrome);
+//        println!("After horizontal run: {:?}", matrix_input);
+        let code_word = vertical_run(&mut matrix_input, &post_proba_log, n, m);
+//        println!("After vertical run: {:?}", matrix_input);
+//        println!("codeword after modification: {:?}", code_word);
+//        println!("syndrome: {:?}", syndrome);
+        success = verification(matrix, &code_word, &syndrome,m);
         match success{
             true => {
                 println!("Number of iteration :{}",_i);
-                return Some(code_word)
+                return Some(code_word);
             }
             false => continue
         }
@@ -131,8 +138,8 @@ fn horizontal_run(matrix: &mut CsMat<f64>, syndrome: &Vec<usize>) {
     for index in 0..m{
         let temp: f64 = data[indptr_clone[index]..indptr_clone[index+1]].iter().product();
         for _i in indptr_clone[index]..indptr_clone[index+1]{
-            let mut temp1 : f64 = 0.0;        
-            if syndrome[index] == 1{
+            let temp1 : f64;        
+            if syndrome[index]%2 == 1{
                 temp1 = -temp/data[_i];
             }else{
                 temp1 = temp/data[_i];
@@ -144,8 +151,8 @@ fn horizontal_run(matrix: &mut CsMat<f64>, syndrome: &Vec<usize>) {
 //    println!("After Horizontal run: {:?}", data);
 }
 
-fn vertical_run(matrix: &mut CsMat<f64>, post_proba: &Vec<f64>, n: usize,m:usize) -> CsVec<usize>{
-    let mut indices = Vec::new();
+fn vertical_run(matrix: &mut CsMat<f64>, post_proba_log: &Vec<f64>, n: usize,m:usize) -> CsVec<usize>{
+    let mut indices: Vec<usize> = Vec::new();
 
     let mut matrix_temp = matrix.to_csc();
     let nnz = matrix.nnz();
@@ -158,7 +165,7 @@ fn vertical_run(matrix: &mut CsMat<f64>, post_proba: &Vec<f64>, n: usize,m:usize
 //    println!("Data before vertical run: {:?}", data);
     for index in 0..n{
         let mut temp: f64 = data[indptr_clone[index]..indptr_clone[index+1]].iter().sum();
-        temp += post_proba[index];
+        temp += post_proba_log[index];
         for _i in indptr_clone[index]..indptr_clone[index+1]{        
             data[_i] = temp - data[_i]
         }
@@ -176,10 +183,21 @@ fn vertical_run(matrix: &mut CsMat<f64>, post_proba: &Vec<f64>, n: usize,m:usize
     CsVec::new(n, indices,data_vec)
 }
 
-fn verification(matrix0: &CsMat<usize>, code_word: &CsVec<usize>, syndrome: &CsVec<usize>) -> bool {
+fn verification(matrix0: &CsMat<usize>, code_word: &CsVec<usize>, syndrome: &CsVec<usize>,m:usize) -> bool {
     // Maybe there's a better way to do this, but here's what I used:
     // https://stackoverflow.com/questions/66925648/how-do-i-create-a-two-dimensional-array-from-a-vector-of-vectors-with-ndarray
-    if matrix0 * code_word == *syndrome{
+    let temp = matrix0 * code_word;
+    let mut new_data : Vec<usize> = Vec::new();
+    let mut new_indices : Vec<usize> = Vec::new();
+    for i in 0..temp.indices().len(){
+        if temp.data()[i] % 2 == 1{
+            new_indices.push(temp.indices()[i]);
+            new_data.push(1);
+        }
+    }
+    let trial = CsVec::new(m,new_indices, new_data);
+//    println!("Trial is : {:?}", trial);
+    if trial == *syndrome{
         true
     }else{false}
 }
@@ -192,26 +210,74 @@ fn verification(matrix0: &CsMat<usize>, code_word: &CsVec<usize>, syndrome: &CsV
 //    }
 //}
 
+pub fn message_passing_test() {
+    let n = 12;
+    let m = 9;
+    let w_c = 3;
+    let w_r = 4;
+    let crossover_proba = 0.2;
+    let mut original_code_word = vec![0,0,1,0,0,0,1,0,0,0,0,1];
+    let mut original_compact_form :Vec<usize> = Vec::new();
+    let mut num_one: usize = 0;
+    let mut rng = thread_rng();
+    let choices: [usize;2] = [0,1];
+    for i in 0..n{
+      original_code_word[i] = *choices.choose(&mut rng).unwrap();
+      if original_code_word[i] == 1{
+         original_compact_form.push(i);
+         num_one += 1;
+      }
+    }
+
+    let sparse_vec = CsVec::new(12,original_compact_form, vec![1;num_one]);
+    println!("{:?}", sparse_vec);
+    let (received, post_proba) = bsc_channel(n, &original_code_word, crossover_proba);
+    println!("After bsc channel: {:?} \n {:?}",received, post_proba);
+    let mut matrix = CsMat::new((9,12),
+                                    vec![0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
+                                    vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 2, 4, 7, 3, 5, 9, 11, 1, 6, 8, 10, 2, 4, 6, 9, 5, 7, 10, 11, 0, 1, 3, 8],
+                                    vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    let post_proba:Vec<f64> = vec![0.8,0.8,0.2,0.8,0.8,0.8,0.2,0.8,0.8,0.8,0.8,0.2];
+    let mut post_proba_log:Vec<f64> = vec![0.0;n];
+    for i in 0..n{
+        post_proba_log[i] = (post_proba[i]/(1.0-post_proba[i])).ln();
+    }
+    let syndrome_tests: CsVec<usize> = CsVec::new(9,vec![0,1,2,3,4,5,7], vec![1,1,1,1,1,1,1]);
+    
+    let mut matrix_input = input_regular_ldpc(m, n, &matrix, &post_proba);
+    let post_process_code_word = message_passing(&mut matrix, syndrome_tests, post_proba, 1);
+
+    //    let syndrome = vec![0,1,0,1,0,0,0,0,1];
+//    horizontal_run(&mut matrix_input, &syndrome);  
+//    println!("{:?}", matrix_input);  
+//    let _ = vertical_run(&mut matrix_input, &post_proba_log, n, m);
+//    println!("{:?}",matrix_input);
+//  [0,1,0,0,1,1,0,1,0,0,0,1]
+    let vec_tests: CsVec<usize> = CsVec::new(12,vec![2,6,11], vec![1,1,1]);
+//    let test_product = &matrix *&vec_tests;
+//    println!("Test product is {:?}", test_product);
+    let syndrome_tests: CsVec<usize> = CsVec::new(9,vec![0,1,2,3,4,5,7], vec![1,1,1,1,1,1,1]);
+    println!("{:?}",verification(&matrix, &vec_tests, &syndrome_tests,m));
+     
+}
 
 #[cfg(test)]
 mod tests{
     use super::*;
-//    #[test]
-//    fn demo() {
-//        let n = 1000000;
-//        let w_c = 4;
-//        let w_r = 8;
-//        let crossover_proba = 0.02;
-//        let seed =10;
-//        let mut original_code_word : Vec<usize> = vec![0;n];
-//        let mut matrix = make_matrix_regular_ldpc(w_c, w_r, n, seed);
-//        let (received, post_proba) = bsc_channel(n, &mut original_code_word, crossover_proba);
-//        let syndrome = &matrix * &received;
-//        match message_passing(&mut matrix, syndrome, post_proba, 60){
-//            Some(_value) => {
-//                assert_eq!(2,2);
-//            }
-//            None => assert_eq!(2,3)
-//        }
-//    }
+    #[test]
+    fn horizontal_run_test() {
+        let n = 12;
+        let m = 9;
+        let w_c = 3;
+        let w_r = 4;
+        let mut matrix = CsMat::new_csc((9,12),
+        vec![0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
+        vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 2, 4, 7, 3, 5, 9, 11, 1, 6, 8, 10, 2, 4, 6, 9, 5, 7, 10, 11, 0, 1, 3, 8],
+        vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+        let post_proba = vec![0.8,0.8,0.2,0.8,0.8,0.8,0.2,0.8,0.8,0.8,0.8,0.2];
+        let mut matrix_input = input_regular_ldpc(m, n, &matrix, &post_proba);
+        let syndrome = vec![0,1,0,1,0,0,0,0,1,1,0,0];
+        horizontal_run(&mut matrix_input, &syndrome);  
+        println!("{:?}", matrix_input);     
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,57 @@
 mod data_processing;
 use sprs::CsVec;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 use std::time::{Duration, Instant};
 
 fn main() {
-    let n = 840000;
-    let w_c = 3;
-    let w_r = 6;
+    let n = 120000;
+    let w_c = 4;
+    let w_r = 8;
     let n = w_r * (n / w_r);
+    let m = n * w_c/w_r;
     println!("n={}", n);
     let crossover_proba = 0.05;
     let seed = 10;
     let mut original_code_word: Vec<usize> = vec![0; n];
-    let sparse_vec = CsVec::new(n, vec![0; 0], vec![1; 0]);
+    let mut original_compact_form: Vec<usize> = vec![];
+    let mut num_one = 0;
+    let mut rng = thread_rng();
+    let choices: [usize;2] = [0,1];
+    for i in 0..n{
+      original_code_word[i] = *choices.choose(&mut rng).unwrap();
+      if original_code_word[i] == 1{
+         original_compact_form.push(i);
+         num_one += 1;
+      }
+    }  
+//    println!("Original codeword is {:?}", original_code_word); 
+    let original_compact_form_clone = original_compact_form.clone();
+    let sparse_vec = CsVec::new(n, original_compact_form, vec![1; num_one]);
     let mut time0 = Instant::now();
-    let (received, post_proba) = data_processing::bsc_channel(n, &mut original_code_word, crossover_proba);
+    let (received, post_proba) = data_processing::bsc_channel(n, &original_code_word, crossover_proba);
     let mut time1 = Instant::now();
     println!("time to generate receive vector: {:?}", time1.saturating_duration_since(time0));
-
-    //   let a = CsMat::new_csc((9,12),
-    //                     vec![0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
-    //                     vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 2, 4, 7, 3, 5, 9, 11, 1, 6, 8, 10, 2, 4, 6, 9, 5, 7, 10, 11, 0, 1, 3, 8],
-    //                     vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-    //   println!("Hello!");
     time0 = Instant::now();
     let mut matrix = data_processing::make_matrix_regular_ldpc(w_c, w_r, n, seed);
     time1 = Instant::now();
     println!("time to generate matrix: {:?}", time1.saturating_duration_since(time0));
-
-    //   println!("matrix indice: {:?}", matrix);
     let syndrome = &matrix * &sparse_vec;
-    //   println!("syndrome: {:?}",syndrome.indices());
-    //   println!("received: {:?}", received);
+    let mut new_data : Vec<usize> = Vec::new();
+    let mut new_indices : Vec<usize> = Vec::new();
+    for i in 0..syndrome.data().len(){
+        if syndrome.data()[i] % 2 == 1{
+            new_indices.push(syndrome.indices()[i]);
+            new_data.push(1);
+        }
+    }
+    let syndrome = CsVec::new(m,new_indices,new_data);
     time0 = Instant::now();
     match data_processing::message_passing(&mut matrix, syndrome, post_proba, 60) {
         Some(value) => {
             println!("That is great but not quite!");
-            //         assert_eq!(value.indices(), original_code_word)
+            assert_eq!(value.indices(), original_compact_form_clone);
         }
         None => {
             println!("Sorry mate!")
@@ -44,6 +59,6 @@ fn main() {
     }
     time1 = Instant::now();
     println!("time to (not) decode: {:?}", time1.saturating_duration_since(time0));
-
+//    data_processing::message_passing_test();
     //   println!("Hello");
 }
